b6317a50c69ce9fe927465d76ae651bc14809714
OmarElFaroukSamine <omarelfarouk.samine@gmail.com>
2024-04-07 16:51:46 +0200

    Changing the way shortcuts are created for better performance and fixing the issue with the shortcuts not working

--- src/main.py
+++ src/main.py
@@ -1,43 +1,58 @@
-import os
+import os, winshell
 import sys
+from win32com.client import Dispatch
 import tkinter as tk
 from tkinter import filedialog,  messagebox
-import pyshortcuts
-from win32com.client import Dispatch
 from pathlib import Path
 
 root = tk.Tk()
 root.withdraw()
 
-cpath = Path(sys.argv[0]).resolve().parent
-icopath = cpath.parent / "data" / "icon.ico"
 
+#Getting the icon from the data folder
+cpath = Path(sys.argv[0]).resolve().parent
+icopath = str(cpath.parent / "data" / "icon.ico")
+
+
+#Assigning it to the tkinter filedialog
 root.iconbitmap(icopath)
 
+
+#Selecting the folder we want to create a workspace from, and where to save this workspace file
 folder_path = filedialog.askdirectory(
     initialdir="/",
     title="Select workspace folder"
 )
-
 save_location = filedialog.askdirectory(
     initialdir="/",
     title="Save workspace"
 )
 
+
+
+#Checking if the user actually selected a valid folder path and save location
 if folder_path and save_location:
+    
+    #Creating the code-workspace file in the save location
     folder_name = os.path.basename(folder_path)
     filepath = os.path.join(save_location, folder_name + ".code-workspace")
-
     with open(filepath, "w") as f:
         content = '{"folders": [{"path": "' + folder_path + '"}],"settings": {}}'
         f.write(content)
 
-    shortcut_name = folder_name
-    pyshortcuts.make_shortcut(
-        filepath,
-        name=shortcut_name,
-        icon=str(icopath),
-        folder=None
-    )
+    
+    #Creating the shortcut on the desktop
+    desktop = winshell.desktop()
+    path = os.path.join(desktop, folder_name + ".lnk")
+    target = filepath
+    wDir = save_location
+    icon = icopath
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(path)
+    shortcut.Targetpath = target
+    shortcut.WorkingDirectory = wDir
+    shortcut.IconLocation = icon
+    shortcut.save()
 else :
+    #Showing a warning if the user didn't select valid workspace folder and save location
     messagebox.showwarning(title="Warning", message="Error while selecting folders")
